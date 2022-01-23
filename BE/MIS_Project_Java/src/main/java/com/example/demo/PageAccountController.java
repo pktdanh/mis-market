fdf4c298bcb37ff7854907d99b60a15e8b8fac76
Namvo-1304 <55818180+Namvo-1304@users.noreply.github.com>
2022-01-23 05:43:49 -0800

    Add Java BackEnd

---
+++ BE/MIS_Project_Java/src/main/java/com/example/demo/PageAccountController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@CrossOrigin
+@Controller
+public class PageAccountController {
+	@RequestMapping(value = "/api/account/all/paging2", method = RequestMethod.POST)
+	public ResponseEntity<List<PageAccount>> All(@RequestBody PagingAccount data) 
+	{
+		 PageAccount paging = new PageAccount();
+		 List<PageAccount> pagings = new ArrayList<PageAccount>();
+		 pagings = paging.getAll(data.AccountPerPage);
+		 return new ResponseEntity<>(pagings, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/account/all/paging2/{per}", method = RequestMethod.GET)
+	public ResponseEntity<List<PageAccount>> All(@PathVariable("per") String per) 
+	{
+	     int temp = Integer.parseInt(per);
+		 PageAccount paging = new PageAccount();
+		 List<PageAccount> pagings = new ArrayList<PageAccount>();
+		 pagings = paging.getAll(temp);
+		 return new ResponseEntity<>(pagings, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/account/all/paging3", method = RequestMethod.GET)
+	public ResponseEntity<PageAccount> All() 
+	{
+		 PageAccount paging = new PageAccount();
+		 paging = paging.get(9, 1);
+		 System.out.println(paging.Accounts.get(0).AccountID);
+		 System.out.println(paging.Accounts.get(1).AccountID);
+		 System.out.println(paging.Accounts.get(2).AccountID);
+		 System.out.println(paging.Accounts.get(3).AccountID);
+		 System.out.println(paging.Accounts.get(4).AccountID);
+		 System.out.println(paging.Accounts.get(5).AccountID);
+		 System.out.println(paging.Accounts.get(6).AccountID);
+		 System.out.println(paging.Accounts.get(7).AccountID);
+		 System.out.println(paging.Accounts.get(8).AccountID);
+		 return new ResponseEntity<>(paging, HttpStatus.OK);
+	}
+}
